cba2b6c311e48bf7e82eeff50ae8761c8ffdb0de
Pavithra <pavithraravi0909@gmail.com>
2021-04-16 22:23:46 +0530

    loading spinner

--- amazon/frontend/src/components/LoadingBox.js
+++ amazon/frontend/src/components/LoadingBox.js
@@ -3,7 +3,7 @@ import React from "react";
 export const LoadingBox = () => {
   return (
     <div className="loading">
-      <i className="fa fa-spinner fa-spin">Loading...</i>
+      <i className="fa fa-spinner fa-spin"></i>Loading...
     </div>
   );
 };
